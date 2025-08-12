1d9e20142d42211ec88fea0a1cc6b14efc8ce4a1
artur-vol <volunetsartur@gmail.com>
2025-08-12 21:30:58 +0300

    Add network resources configuration
    Configured VPC, Internet Gateway, Private and Public Subnets, Private Subnet Group, Route Tables

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,113 @@ provider "aws" {
   region = var.region
 }
 
+# Virtual Private Network
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+# Internet Gateway 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = var.igw_name
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr_block
+  availability_zone       = var.public_subnet_az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.public_subnet_name
+  }
+}
+
+# Private Subnet_1
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidr_block_1
+  availability_zone = var.private_subnet_az_1
+
+  tags = {
+    Name = "${var.private_subnet_name}-1"
+  }
+}
+
+# Private Subnet_2
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidr_block_2
+  availability_zone = var.private_subnet_az_2
+
+  tags = {
+    Name = "${var.private_subnet_name}-2"
+  }
+}
+
+# Private Subnet Group
+resource "aws_db_subnet_group" "this" {
+  name = "${var.vpc_name}-db-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id,
+  ]
+
+  tags = {
+    Name = "${var.vpc_name}-db-subnet-group"
+  }
+}
+
+
+# Public Subnet Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = var.public_route_table_name
+  }
+}
+
+# Private Subnets Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = var.private_route_table_name
+  }
+}
+
+# Public Subnet and Route Table Association
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Subnet_1 and Route Table Association
+resource "aws_route_table_association" "private_subnet_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+# Private Subnet_2 and Route Table Association
+resource "aws_route_table_association" "private_subnet_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
+
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,100 @@
 # variables.tf
 
+# Provider
+
 variable "region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "eu-central-1"
+  default     = "eu-central-1" 
 }
+
+# VPC
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name tag for the VPC"
+  type        = string
+  default     = "provedcode-vpc"
+}
+
+# Internet Gateway
+
+variable "igw_name" {
+  description = "Name tag for the Internet Gateway"
+  type        = string
+  default     = "my_igw"
+}
+
+# Public Subnet
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_az" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "eu-central-1a"
+}
+
+variable "public_subnet_name" {
+  description = "Name tag for the public subnet"
+  type        = string
+  default     = "public_subnet"
+}
+
+# Private Subnets
+
+
+variable "private_subnet_az_1" {
+  description = "Availability Zone for the first private subnet"
+  type        = string
+  default     = "eu-central-1a"
+}
+
+variable "private_subnet_az_2" {
+  description = "Availability Zone for the second private subnet"
+  type        = string
+  default     = "eu-central-1b"
+}
+
+variable "private_subnet_cidr_block_1" {
+  description = "CIDR block for the first private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_cidr_block_2" {
+  description = "CIDR block for the second private subnet"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+
+variable "private_subnet_name" {
+  description = "Name tag for the private subnet"
+  type        = string
+  default     = "private_subnet"
+}
+
+# Route Table
+variable "public_route_table_name" {
+  description = "Name tag for the public route table"
+  type        = string
+  default     = "public_route_table"
+}
+
+variable "private_route_table_name" {
+  description = "Name tag for the private route table"
+  type        = string
+  default     = "private_route_table"
+}
+
 

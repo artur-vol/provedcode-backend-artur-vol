da8a23c66c6b56b801af8280f4d22a5286d51828
artur-vol <volunetsartur@gmail.com>
2025-08-21 20:43:54 +0300

    Add pipeline base

--- jenkins/provedcode-terraform-test.groovy
+++ jenkins/provedcode-terraform-test.groovy
@@ -1,23 +1,16 @@
-# JENKINSFILE
-
 pipeline {
-    agent any
+  agent any
 
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-    }
+  parameters {
+
+  }
+
+  environment {
+
+  }
+
+  stages {
+
+  }
+
 }

aa4b6c6cc27ca1f3ac1aa104dc2928820d129875
artur-vol <volunetsartur@gmail.com>
2025-08-21 21:02:11 +0300

    Move region from creds to params

--- jenkins/provedcode-terraform-test.groovy
+++ jenkins/provedcode-terraform-test.groovy
@@ -2,13 +2,13 @@ pipeline {
   agent any
 
   parameters {
-
+    string(name: 'REGION', defaultValue: 'eu-central-1', description: 'AWS Region for deployment')
   }
 
   environment {
     AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
     AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-    AWS_DEFAULT_REGION    = credentials('aws-region')
+    AWS_DEFAULT_REGION    = "${params.REGION}" 
   }
 
   stages {

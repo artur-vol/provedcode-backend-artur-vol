e7ed2f67dedef5fb1b307c1c83383cf86f745434
artur-vol <volunetsartur@gmail.com>
2025-08-21 21:25:34 +0300

    Add terraform initialisation and state list checking

--- jenkins/provedcode-terraform-test.groovy
+++ jenkins/provedcode-terraform-test.groovy
@@ -1,24 +1,34 @@
 pipeline {
-  agent any
-
-  parameters {
-    string(name: 'REGION', defaultValue: 'eu-central-1', description: 'AWS Region for deployment')
-  }
-
-  environment {
-    AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
-    AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-    AWS_DEFAULT_REGION    = "${params.REGION}" 
-  }
-
-  stages {
-
-    stage('Checkout') {
-      steps {
-        git branch: 'terraform', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
-      }
+    agent any
+    parameters {
+        string(name: 'REGION', defaultValue: 'eu-central-1', description: 'AWS Region for deployment')
     }
-    
-  }
-
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+        AWS_DEFAULT_REGION = "${params.REGION}"
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'terraform', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
+            }
+        }
+        stage('Terraform Init') {
+            steps {
+                dir('terraform') {
+                    sh 'terraform init'
+                }
+            }
+        }
+        stage('Check Remote State') {
+            steps {
+                dir('terraform') {
+                    sh 'terraform state list'
+                }
+            }
+        }
+    }
 }
+
+

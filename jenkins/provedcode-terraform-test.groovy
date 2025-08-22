4a6df59891e5c64d805714e0c7f77424503f5867
artur-vol <volunetsartur@gmail.com>
2025-08-22 21:20:42 +0300

    Break down into stages

--- jenkins/provedcode-terraform-test.groovy
+++ jenkins/provedcode-terraform-test.groovy
@@ -4,34 +4,41 @@ pipeline {
         string(name: 'REGION', defaultValue: 'eu-central-1', description: 'AWS Region for deployment')
     }
     environment {
-        AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
+        AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-        AWS_DEFAULT_REGION = "${params.REGION}"
+        AWS_DEFAULT_REGION    = "${params.REGION}"
     }
     stages {
-        // FRONTEND
-        stage('Checkout Frontend Repo') {
-          steps {
-            git branch: 'main', url: 'https://github.com/artur-vol/provedcode-frontend-artur-vol.git'
-          }
-        }
-        // TERRAFORM
-        stage('Checkout') {
-            steps {
-                git branch: 'terraform', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
-            }
-        }
-        stage('Terraform Init') {
-            steps {
-                dir('terraform') {
-                    sh 'terraform init'
+        stage('Frontend') {
+            stages {
+                stage('Checkout Frontend Repo') {
+                    steps {
+                        git branch: 'main', url: 'https://github.com/artur-vol/provedcode-frontend-artur-vol.git'
+                    }
                 }
             }
         }
-        stage('Check Remote State') {
-            steps {
-                dir('terraform') {
-                    sh 'terraform state list'
+
+        stage('Terraform') {
+            stages {
+                stage('Checkout') {
+                    steps {
+                        git branch: 'terraform', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
+                    }
+                }
+                stage('Terraform Init') {
+                    steps {
+                        dir('terraform') {
+                            sh 'terraform init'
+                        }
+                    }
+                }
+                stage('Check Remote State') {
+                    steps {
+                        dir('terraform') {
+                            sh 'terraform state list'
+                        }
+                    }
                 }
             }
         }

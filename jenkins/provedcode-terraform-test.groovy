53e851ea19bf95b48e7f5532ebff68fdc7068705
artur-vol <volunetsartur@gmail.com>
2025-08-23 16:00:45 +0300

    Fix terraform

--- jenkins/provedcode-terraform-test.groovy
+++ jenkins/provedcode-terraform-test.groovy
@@ -1,76 +1,22 @@
 pipeline {
+
   agent any
+
   tools {
     nodejs 'NodeJS_22'
   }
+
   parameters {
     string(name: 'REGION', defaultValue: 'eu-central-1', description: 'AWS Region for deployment')
   }
+
   environment {
     AWS_ACCESS_KEY_ID = credentials('aws-access-key-id')
     AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
     AWS_DEFAULT_REGION = "${params.REGION}"
   }
+
   stages {
-    stage('Backend') {
-      stages {
-        stage('Checkout') {
-          steps {
-            git branch: 'main', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
-          }
-        }
-        stage('Configure Permissions') {
-          steps {
-            sh 'chmod +x ./mvnw'
-          }
-        }
-        stage('Download Dependencies') {
-          steps {
-            sh './mvnw dependency:go-offline'
-          }
-        }
-        stage('Test') {
-          steps {
-            sh './mvnw test'
-          }
-        }
-        stage('Build') {
-          steps {
-            sh './mvnw clean package -DskipTests'
-          }
-          post {
-            success {
-              archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
-            }
-          }
-        }
-      }
-    }
-    stage('Frontend') {
-      stages {
-        stage('Checkout Frontend Repo') {
-          steps {
-            git branch: 'main', url: 'https://github.com/artur-vol/provedcode-frontend-artur-vol.git'
-          }
-        }
-        stage('Download Dependencies') {
-          steps {
-            sh 'npm ci'
-          }
-        }
-        stage('Build Frontend') {
-          steps {
-            sh 'npm run build'
-            sh 'tar -cvf "build.tar" build/'
-          }
-          post {
-            success {
-              archiveArtifacts artifacts: 'build.tar', fingerprint: true
-            }
-          }
-        }
-      }
-    }
 
     stage('Terraform') {
       stages {
@@ -93,6 +39,78 @@ pipeline {
             }
           }
         }
+        stage('Terraform Plan') {
+          steps {
+            dir('terraform') {
+              sh 'terraform plan -out=tfplan'
+            }
+          }
+          stage('Terraform Apply') {
+
+          }
+        }
       }
+
+      stage('Backend') {
+        stages {
+          stage('Checkout') {
+            steps {
+              git branch: 'main', url: 'https://github.com/artur-vol/provedcode-backend-artur-vol.git'
+            }
+          }
+          stage('Configure Permissions') {
+            steps {
+              sh 'chmod +x ./mvnw'
+            }
+          }
+          stage('Download Dependencies') {
+            steps {
+              sh './mvnw dependency:go-offline'
+            }
+          }
+          stage('Test') {
+            steps {
+              sh './mvnw test'
+            }
+          }
+          stage('Build') {
+            steps {
+              sh './mvnw clean package -DskipTests'
+            }
+            post {
+              success {
+                archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+              }
+            }
+          }
+        }
+      }
+
+      stage('Frontend') {
+        stages {
+          stage('Checkout Frontend Repo') {
+            steps {
+              git branch: 'main', url: 'https://github.com/artur-vol/provedcode-frontend-artur-vol.git'
+            }
+          }
+          stage('Download Dependencies') {
+            steps {
+              sh 'npm ci'
+            }
+          }
+          stage('Build Frontend') {
+            steps {
+              sh 'npm run build'
+              sh 'tar -cvf "build.tar" build/'
+            }
+            post {
+              success {
+                archiveArtifacts artifacts: 'build.tar', fingerprint: true
+              }
+            }
+          }
+        }
+      }
+
     }
   }

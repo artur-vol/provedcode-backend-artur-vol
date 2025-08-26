95dd5c4b2506c6426ead0da3a163ebe4d07a9b27
artur-vol <volunetsartur@gmail.com>
2025-08-27 00:15:02 +0300

    Fix bash script error

--- ansible/scripts/key_setup.sh
+++ ansible/scripts/key_setup.sh
@@ -4,7 +4,7 @@ mkdir -p "${WORKSPACE}/.ssh"
 chmod 700 "${WORKSPACE}/.ssh"
 > "${WORKSPACE}/.ssh/known_hosts"
 
-cp "$EDGE_KEY"    "${WORKSPACE}/.ssh/edge_gateway_key.pem"
-cp "$BACKEND_KEY" "${WORKSPACE}/.ssh/backend_key.pem"
-cp "$FRONTEND_KEY"${WORKSPACE}/.ssh/frontend_key.pem"
+cp "$EDGE_KEY"     "${WORKSPACE}/.ssh/edge_gateway_key.pem"
+cp "$BACKEND_KEY"  "${WORKSPACE}/.ssh/backend_key.pem"
+cp "$FRONTEND_KEY" "${WORKSPACE}/.ssh/frontend_key.pem"
 chmod 600 "${WORKSPACE}"/.ssh/*.pem
